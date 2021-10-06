167586b20b389e1b57f39ee2edbfcbbe27c61eb7
Bill Muxworthy <bill.muxworthy@doctorcareanywhere.com>
2021-10-06 09:47:32 +0100

    Add styles for main page

--- src/StartQuiz.tsx
+++ src/StartQuiz.tsx
@@ -1,4 +1,4 @@
-
+import "./styles/StartQuiz.css"
 
 interface IProps {
 
@@ -7,9 +7,17 @@ interface IProps {
 const StartQuiz = (props: IProps) => {
 	return (
 		<>
-			<h2>Hello</h2>
+			<h2>Here comes the quiz</h2>
+			<h3>Hello quiz</h3>
+			<h4>Welcome</h4>
+			<h5>Good luck</h5>
+			<a onClick={start}>There it is</a>
 		</>
 	)
+}
+
+function start () {
+	
 }
 
 export default StartQuiz
